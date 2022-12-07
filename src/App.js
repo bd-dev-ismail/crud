4b76150167f657deddbdf7ddbbd3af0a29deba27
bd-dev-ismail <ismailratul7@gmail.com>
2022-12-08 00:27:54 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [users, setUsers] = useState({});
+  const [displayUsers, setDisplayUsers] = useState([]);
+  const handalSubmit = (e) =>{
+    e.preventDefault();
+
+  }
+  const handalBlur = (e)=>{
+    const field = e.target.name;
+    const value = e.target.value;
+    const newUser = {...users};
+    newUser[field] = value;
+    setUsers(newUser)
+  }
+  //get data
+  useEffect(()=>{
+    fetch("http://localhost:5000/users")
+    .then(res => res.json())
+    .then(data => setDisplayUsers(data))
+    .catch(err => console.error(err))
+  },[])
+  useEffect(() => {
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(users),
+    })
+      .then((res) => res.json())
+      .catch((data) => setUsers(data));
+  }, [users]);
+  const handalDelete =(users)=>{
+    console.log(users._id);
+    const agreed = window.confirm("Are you sure to delete ?");
+    if(agreed){
+      fetch(`http://localhost:5000/users/${users._id}`, {
+        method: 'DELETE'
+      })
+      .then(res => res.json())
+      .then(data => {
+        if(data.deletedCount){
+          alert('Successfully Deleted')
+          const remaning = displayUsers.filter(usr => usr._id !== users._id);
+          setDisplayUsers(remaning)
+        }
+      })
+
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Add a user: </h2>
+      <form onSubmit={handalSubmit}>
+        <input
+          onBlur={handalBlur}
+          type="text"
+          name="name"
+          id=""
+          placeholder="Enter Your Name"
+          required
+        />
+        <br />
+        <input
+          onBlur={handalBlur}
+          type="email"
+          name="email"
+          id=""
+          placeholder="Enter Your Email"
+          required
+        />
+        <br />
+        <button type="submit">Add User</button>
+      </form>
     </div>
   );
 }
---
+++ src/component/AddUsers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddUsers = () => {
+    return (
+        <div>
+            <h2>Please Add a user: </h2>
+            <p>Create a User</p>
+        </div>
+    );
+};
+
+export default AddUsers;
